83bc056046f2a8710676806b5751cf58425d21be
Maxim Kirichenko <kirichenkomaxim@icloud.com>
2023-01-16 13:52:30 +0300

    add: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [];
+  const bracketsPair = {};
+  const bracketsTwins = [];
+  let stack = [];
+
+  bracketsConfig.forEach(element => {
+    openBrackets.push(element[0]);
+    bracketsPair[element[1]] = element[0];
+    if (element[0] === element[1]) {
+      bracketsTwins.push(element[0]);
+    }
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+
+    if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+      if (currentSymbol === topElement && bracketsTwins.includes(currentSymbol)) {
+        stack.pop();
+        stack.pop();
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      };
+
+      if (bracketsPair[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      };
+    };
+  };
+  return stack.length === 0;
 }
